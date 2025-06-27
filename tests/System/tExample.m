4e94b55a276d7aa69ae28acf9011981186913e84
littleCodeMaster <34719598+littleCodeMaster2021@users.noreply.github.com>
2025-06-27 11:50:41 +0100

    try 6

--- tests/System/tExample.m
+++ tests/System/tExample.m
@@ -76,6 +76,7 @@ classdef tExample < matlab.uitest.TestCase
                 test.App.AccuracyTab.Title,'Accuracy');
             disp('9');
             % Compute Network Accuracy
+
             %test.pushButton(test.App.AccuracyButton);
             disp('10');
             % Verify the accuracy table having correct format of data
