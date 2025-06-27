b232ba231332e217e3ad723fc65fe5c5de68a276
littleCodeMaster <34719598+littleCodeMaster2021@users.noreply.github.com>
2025-06-27 12:13:44 +0100

    try
     try

--- tests/System/tExample.m
+++ tests/System/tExample.m
@@ -1,4 +1,4 @@
-classdef tExample < matlab.uitest.TestCase
+classdef tExample < matlab.unittest.TestCase
     properties(Access = private)
         App
         ImdsVal
@@ -76,7 +76,7 @@ classdef tExample < matlab.uitest.TestCase
                 test.App.AccuracyTab.Title,'Accuracy');
             disp('9');
             % Compute Network Accuracy
-            test.App.AccuracyButtonPushedFunc();
+            test.App.runAccuracyButtonPushedCallback();
             %test.pushButton(test.App.AccuracyButton);
             disp('10');
             % Verify the accuracy table having correct format of data
