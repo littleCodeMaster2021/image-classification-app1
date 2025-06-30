591e4bb845b4466a8a760750c844cf7960b46679
littleCodeMaster <34719598+littleCodeMaster2021@users.noreply.github.com>
2025-06-30 14:35:57 +0100

    Update tUNPICHeadless.m

--- tests/System/tUNPICHeadless.m
+++ tests/System/tUNPICHeadless.m
@@ -37,18 +37,22 @@ classdef tUNPICHeadless < matlab.uitest.TestCase
 
         function trainNet(test)
             % Load a pretrained GoogLeNet network
+            fprintf('Load a pretrained GoogLeNet network!\n');
             net = googlenet;
            
             % Resize the images to the input size of the pretrained network.
+            fprintf('Resize the images to the input size of the pretrained network!\n');
             inputSize = net.Layers(1).InputSize;
 
             augimdsTrain = augmentedImageDatastore(inputSize,test.ImdsTrain);
             augimdsVal = augmentedImageDatastore(inputSize,test.ImdsVal);
 
             %Define Network Architecture
+            fprintf('Define Network Architecture!\n');
             lgraph = layerGraph(net);
             
             %Define new layers specific to this data set.
+            fprintf('Define new layers specific to this data set!\n');
             numClasses = numel(categories(test.ImdsTrain.Labels));
             newLearnableLayer = fullyConnectedLayer(numClasses, ...
                 'Name','new_fc', ...
@@ -56,12 +60,14 @@ classdef tUNPICHeadless < matlab.uitest.TestCase
                 'BiasLearnRateFactor',10);
 
             newClassLayer = classificationLayer('Name','new_classoutput');
-            
+
             % Replace the current layers with the new layers.
+            fprintf('Replace the current layers with the new layers!\n');
             lgraph = replaceLayer(lgraph,'loss3-classifier',newLearnableLayer);
             lgraph = replaceLayer(lgraph,'output',newClassLayer);
 
             %Train Network
+            fprintf('Train Network!\n');
             valFrequencyEpochs = 1;
             miniBatchSize = 50;
 
